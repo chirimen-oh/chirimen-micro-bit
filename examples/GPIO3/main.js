049a3d1ef8ee6fa02bea9965cf7ba2b77d8718cb
Satoru Takagi <satakagi@gmail.com>
2020-07-31 12:20:00 +0900

    Change imgs of examples

--- examples/GPIO3/main.js
+++ examples/GPIO3/main.js
@@ -1,14 +1,14 @@
 var microBitBle;
 
-var gpioPort1;
+var gpioPort0;
 
 async function connect(){
 	microBitBle = await microBitBleFactory.connect();
 	msg.innerHTML=("micro:bit BLE接続しました。");
 	var gpioAccess = await microBitBle.requestGPIOAccess();
 	var mbGpioPorts = gpioAccess.ports;
-	gpioPort1 = mbGpioPorts.get(1);
-	await gpioPort1.export("out"); //port1 out
+	gpioPort0 = mbGpioPorts.get(0);
+	await gpioPort0.export("out"); //port0 out
 }
 
 async function disconnect(){
@@ -18,9 +18,9 @@ async function disconnect(){
 
 async function motorControl(val){
 	if ( val == true ){
-		await gpioPort1.write(1);
+		await gpioPort0.write(1);
 	} else {
-		await gpioPort1.write(0);
+		await gpioPort0.write(0);
 	}
 	msg.innerHTML= "モーター駆動状態 : "+val ;
 }
