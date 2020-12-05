a3a0f19035e718d712ea3e35ce4f3535c70a8477
jyoi360 <74086520+jyoi360@users.noreply.github.com>
2020-12-06 06:58:03 +0900

    initial commit

--- examples/I2C3_VL53L0X/main.js
+++ examples/I2C3_VL53L0X/main.js
@@ -1,5 +1,6 @@
 var microBitBle;
-
+var gpioPort0;
+var gpioPort1
 var vl53;
 
 var readEnable;
@@ -14,6 +15,14 @@ async function connect(){
 	await vl53.init();
 	readEnable = true;
 	readData();
+	//↑距離センサーの初期設定
+	//↓モーターの初期設定
+	var gpioAccess = await microBitBle.requestGPIOAccess();
+  var mbGpioPorts = gpioAccess.ports;
+  gpioPort0 = mbGpioPorts.get(0);
+  await gpioPort0.export("out"); //port0 out
+  gpioPort1 = mbGpioPorts.get(1);
+  await gpioPort1.export("out"); // port1 out
 }
 
 async function disconnect(){
@@ -21,13 +30,50 @@ async function disconnect(){
 	await microBitBle.disconnect();
 	msg.innerHTML=("micro:bit BLE接続を切断しました。");
 }
-
 async function readData(){
+	var rot = 0;//0か1かで回転方向を指定
+	var gpio0Val = 0;
+	gpio0Val = 0;
+	gpio0Val = rot===0 ? 0:1;
+	
 	while ( readEnable ){
 		var distance = await vl53.getRange();
+		var speed = 10;//0〜10までの数値で速度を変化させる。
+		var time = 100;//１判定にかかる時間を設定（単位はミリ秒）
+		var Hspeed = 10;//
+		var Htime = 100; 
+		distance = rot === 0 ? distance :2500-distance;
 		console.log("distance:",distance);
 		msg.innerHTML= distance + "mm";
-		await sleep(1000);
+		switch (distance){
+			case distance <= 150:
+				var i = 0;
+				for (i;time;i++){
+				await gpioPort0.write(gpio0Val);
+				await gpioPort1.write(!gpio0Val);
+				await sleep(speed);
+				await gpioPort0.write(0);
+				await gpioPort1.write(0);
+				await sleep(10-speed);
+				}
+				break;
+			case distance < 150 && 200<distance:
+				for (i;Htime;i++){
+	  		await gpioPort0.write(gpio0Val);
+				await gpioPort1.write(!gpio0Val);
+				await sleep(Hspeed);
+				await gpioPort0.write(0);
+				await gpioPort1.write(0);
+				await sleep(10-Hspeed);
+					}	
+			break;
+		   default:
+				await gpioPort0.write(0);
+				await gpioPort1.write(0);
+		}
+
+
+		await sleep(100);
 	}
 }
 
